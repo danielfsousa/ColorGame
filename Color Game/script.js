2b60014844189e9669ea3c6d71737598f5f11c8a
Daniel Sousa <sousa.dfs@gmail.com>
2016-07-25 12:12:47 -0300

    Refatora o código

--- Color Game/script.js
+++ Color Game/script.js
@@ -1,74 +1,86 @@
-var cores = gerarCoresAleatorias(6);
-var numQuadrados = 6;
+//VARS
+var numQuadrados = 9;
+var cores = gerarCoresAleatorias(numQuadrados);
 
 var quadrados = document.querySelectorAll(".quadrado");
 var corEscolhida = escolherCor();
+var h1 = document.querySelector("h1");
 var rgbCor = document.querySelector("#rgbCor");
 var mensagem = document.querySelector("#mensagem");
-var btnReset = document.querySelector("#resetar");
-var btnFacil = document.querySelector("#btnFacil");
-var btnDificil = document.querySelector("#btnDificil");
+var btnResetar = document.querySelector("#resetar");
+var dificuldades = document.querySelectorAll(".dificuldade");
+var btnFacil = dificuldades[0];
+var btnDificil = dificuldades[1];
 
+//INICIAR
+iniciar();
+
+//EVENTOS
 btnFacil.addEventListener("click", function() {
     btnFacil.classList.add("ativo");
     btnDificil.classList.remove("ativo");
-    cores = gerarCoresAleatorias(numQuadrados);
-    corEscolhida = escolherCor();
-    rgbCor.textContent = corEscolhida;
-    for(var i = 0; i < quadrados.length; i++) {
-        if(cores[i]) {
-            quadrados[i].style.background = cores[i];
-            quadrados[i].style.display = "none";
-        }
-    }
+    numQuadrados = 3;
+    resetar();
 });
 
 btnDificil.addEventListener("click", function() {
     btnFacil.classList.remove("ativo");
     btnDificil.classList.add("ativo");
-    numQuadrados = 6;
-    cores = gerarCoresAleatorias(numQuadrados);
-    corEscolhida = escolherCor();
-    rgbCor.textContent = corEscolhida;
-    for(var i = 0; i < quadrados.length; i++) {
-        quadrados[i].style.background = cores[i];
-        quadrados[i].style.display = "none";
-    }
+    numQuadrados = 9;
+    resetar();
 });
 
-btnReset.addEventListener("click", function() {
-    cores = gerarCoresAleatorias(numQuadrados);
-    corEscolhida = escolherCor();
+btnResetar.addEventListener("click", function() {
+    resetar();
+});
+
+//FUNÇÕES
+function iniciar() {
     rgbCor.textContent = corEscolhida;
 
     for(var i = 0; i < quadrados.length; i++) {
         quadrados[i].style.background = cores[i];
+
+        quadrados[i].addEventListener("click", function() {
+            var corClicada = this.style.backgroundColor;
+            console.log(corClicada);
+            if(corClicada === corEscolhida) {
+                btnResetar.textContent = "Jogar Novamente";
+                mensagem.textContent = "Acertou!";
+                mudarCores(corClicada);
+            } else {
+                this.style.background = "#232323";
+                mensagem.textContent = "Tente Novamente"
+            }
+        });
     }
-});
+}
 
-rgbCor.textContent = corEscolhida;
+function resetar() {
+    cores = gerarCoresAleatorias(numQuadrados);
+    corEscolhida = escolherCor();
+    rgbCor.textContent = corEscolhida;
+    mensagem.textContent = "";
 
-for(var i = 0; i < quadrados.length; i++) {
-    quadrados[i].style.background = cores[i];
-
-    quadrados[i].addEventListener("click", function() {
-        var corClicada = this.style.backgroundColor;
-        console.log(corClicada);
-        if(corClicada === corEscolhida) {
-            btnReset.textContent = "Jogar Novamente";
-            mensagem.textContent = "Acertou!";
-            mudarCores(corClicada);
+    for(var i = 0; i < quadrados.length; i++) {
+        if(cores[i]) {
+            quadrados[i].style.display = "block";
+            quadrados[i].style.background = cores[i];
         } else {
-            this.style.background = "#232323";
-            mensagem.textContent = "Tente Novamente"
+            quadrados[i].style.display = "none";
         }
-    });
+    }
+
+    h1.style.background = "#3c7cb4";
+    rgbCor.style.background = "steelblue";
 }
 
 function mudarCores(cor) {
     for(var i = 0; i < cores.length; i++) {
         quadrados[i].style.background = cor;
     }
+    h1.style.background = cor;
+    rgbCor.style.background = cor;
 }
 
 function escolherCor() {
